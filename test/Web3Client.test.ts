73f393257e5bfd1cfdcb4a8e9a43919f5d5769d0
haofengjiang <haofeng.jiang@cobo.com>
2023-03-15 16:32:07 +0800

    drop apikey

--- test/Web3Client.test.ts
+++ test/Web3Client.test.ts
@@ -1,19 +1,20 @@
 import {LocalSigner} from "../src/LocalSigner";
 import {Web3Client} from "../src/Web3Client";
 import {SANDBOX} from "./config";
+import {PROD} from "./config";
 
-var apiSecret:string = 'c421f5d9aa1598e3ad1220e172e50f4b50895cd09d47994f48200e1a5521da62';
+var apiSecret:string = 'apiSecret';
 var clientEnv:any = SANDBOX;
 
-// if(process.argv.length > 3){
-//     const paramEnv = process.argv.filter((x) => x.startsWith('-env='))[0].split('=')[1];
-//     const env = paramEnv ? paramEnv : 'sandbox';
-//     clientEnv = env==='prod' ? PROD: SANDBOX;
-//     const paramApiSecret = process.argv.filter((x) => x.startsWith('-secretKey='))[0].split('=')[1]
-//     apiSecret = paramApiSecret ? paramApiSecret: 'apiSecret' 
-// }
+if(process.argv.length > 3){
+    const paramEnv = process.argv.filter((x) => x.startsWith('-env='))[0].split('=')[1];
+    const env = paramEnv ? paramEnv : 'sandbox';
+    clientEnv = env==='prod' ? PROD: SANDBOX;
+    const paramApiSecret = process.argv.filter((x) => x.startsWith('-web3SecretKey='))[0].split('=')[1]
+    apiSecret = paramApiSecret ? paramApiSecret: 'apiSecret' 
+}
 
-// jest.setTimeout(10000);
+jest.setTimeout(10000);
 const signer = new LocalSigner(apiSecret);
 const web3_client = new Web3Client(signer, clientEnv, false);
 
