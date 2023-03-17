fbf42562efd47eef83c77a383148a68a2322c8db
haofengjiang <haofeng.jiang@cobo.com>
2023-03-17 13:24:03 +0800

    add web3Client import

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import {Client, StakingQueryParams, TransactionQueryParams, WithdrawParams} from "./src/Client";
 import {LocalSigner} from "./src/LocalSigner";
+import {Web3Client} from "./src/Web3Client";
 
 import {PROD, SANDBOX} from "./src/Env";
 import { ApiResponse } from "./src/Base";
 
 export {SANDBOX, PROD}
 
-export {Client, LocalSigner};
+export {Client, LocalSigner, Web3Client};
 export {
     TransactionQueryParams,
     WithdrawParams,
